a63642ba139d97dcd7181bb1f857f46f4af79292
moon0331 <moon0331@skku.edu>
2018-06-24 02:31:26 +0900

    instruction 처리

---
+++ instruction.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+typedef enum { LD, ST, ADD, MUL, LT, CALL, JUMPF, JUMPT, JUMP, MV } Inst;
+
+class Instruction{
+	string getInstruction(Inst inst, string x, string y, string z);
+};
