bb5dda107afa1c5a1b32410f2bffc704bbc72b70
moon0331 <moon0331@skku.edu>
2018-06-23 18:24:07 +0900

    for constant

---
+++ constant.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+using namespace std;
+
+namespace constant {
+	const string reserved[] = { "IF", "THEN", "ELSE", "WHILE", "RETURN" };
+	const string vtype[] = { "INT", "CHAR" };
+}
