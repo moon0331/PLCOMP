c38d583b8444820f6a9b2a6ca6306e7d37e849ce
moon0331 <moon0331@skku.edu>
2018-06-24 02:42:27 +0900

    delete

--- constant.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-using namespace std;
-
-namespace constant {
-	const string reserved[] = { "IF", "THEN", "ELSE", "WHILE", "RETURN" };
-	const string vtype[] = { "INT", "CHAR" };
-}
