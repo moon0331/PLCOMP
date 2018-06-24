ba6520e14937ea8d8e426b0719eb600e2471183a
moon0331 <moon0331@skku.edu>
2018-06-24 09:26:57 +0900

    명령어 불러오는 함수 오버로딩

--- instruction.cpp
+++ instruction.cpp
@@ -6,27 +6,37 @@ using namespace std;
 
 //typedef enum { LD, ST, ADD, MUL, LT, CALL, JUMPF, JUMPT, JUMP, MV } Inst;
 
-string Instruction::getInstruction(Inst inst, string x, string y, string z) {
+string Instruction::getInstruction(Inst inst, string x) {
+	switch (inst) {
+	case CALL:
+		return "CALL\t" + x;
+	case JUMP : 
+		return "JUMP\t" + x;
+	}
+}
+
+string Instruction::getInstruction(Inst inst, string x, string y) {
 	switch (inst) {
 	case LD:
 		return "LD\t" + x + ", " + y;
 	case ST:
 		return "ST\t" + x + ", " + y;
+	case JUMPF:
+		return "JUMPF\t" + x + "\t" + y;
+	case JUMPT:
+		return "JUMPT\t" + x + "\t" + y;
+	case MV:
+		return "MV\t" + x + ", " + y;
+	}
+}
+
+string Instruction::getInstruction(Inst inst, string x, string y, string z) {
+	switch (inst) {
 	case ADD:
 		return "ADD\t" + x + ", " + y + ", " + z;
 	case MUL:
 		return "MUL\t" + x + ", " + y + ", " + z;
 	case LT:
 		return "LT\t" + x + ", " + y + ", " + z;
-	case CALL:
-		return "CALL\t" + x;
-	case JUMPF:
-		return "JUMPF\t" + x + "\t" + y;
-	case JUMPT:
-		return "JUMPT\t" + x + "\t" + y;
-	case JUMP:
-		return "JUMP\t" + x;
-	case MV:
-		return "MV\t" + x + ", " + y;
 	}
 }
